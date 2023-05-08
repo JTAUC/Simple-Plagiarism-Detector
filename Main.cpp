09135ef7c677b741108b602e0073e81e92a03090
JTAUC <jt@aucegypt.edu>
2023-05-08 23:22:23 +0300

    t

--- Main.cpp
+++ Main.cpp
@@ -8,6 +8,8 @@ vector<Match> BF_exact_matches, RK_exact_matches;
 BruteForceMatcher BFMatcher;
 RabinKarp RabinKarpMatcher;
 
+float BF_PlagiarizedCharCount = 0, RK_PlagiarizedCharCount = 0;
+
 //Calculates hamming distance between each sentence in two documents, 
 //if it is less than a given constant it is considered plagiarism.
 //Function returns number of plagiarized characters.
@@ -28,12 +30,13 @@ void BruteForce(Document d1, Document d2) {
     
     vector<string> d1_sentences = d1.getSentences(), d2_sentences = d2.getSentences();
 
-    cout << "Hamming Dists.: " << (GetHammingDistances(d1_sentences, d2_sentences) / d1.getFullText().length()) * 100 << endl;
+    cout << "Hamming Distance: " << (GetHammingDistances(d1_sentences, d2_sentences) / d1.getFullText().length()) * 100 << endl;
     
     for (int i = 0; i < d2.getSentences().size(); i++) { //Sentence Matching
         int index = BFMatcher.runDetection(d1, d2_sentences[i]);
         if (index != -1) {
             BF_exact_matches.push_back(Match(i, index, d2_sentences[i], d1.getFileName()));
+            BF_PlagiarizedCharCount += d2_sentences[i].length();
         }
     }
 }
@@ -46,6 +49,7 @@ void RabinKarpFunc(Document d1, Document d2) {
         int index = 0;
         if (RabinKarpMatcher.runDetection(d1.getFullText(), d2_sentences[i], index)) {
             RK_exact_matches.push_back(Match(i, index, d2_sentences[i], d1.getFileName()));
+            RK_PlagiarizedCharCount += d2_sentences[i].length();
         }
     }
 }
@@ -64,9 +68,11 @@ int main()
 
     for (int i = 0; i < corpus.size(); i++) {
         BruteForce(corpus[i], Plagiarized);
+        cout << "\n";
         RabinKarpFunc(Plagiarized, corpus[i]);
     }
-        
+
+    float totalCharCount = Plagiarized.getFullText().length();
 
     for (int i = 0; i < BF_exact_matches.size(); i++) {
         cout << "(BF) Exact Match Found: " << BF_exact_matches[i].GetText() << " At: " << BF_exact_matches[i].GetCharIndex() << " From: " << BF_exact_matches[i].GetDocumentName() << endl;
@@ -77,4 +83,17 @@ int main()
         cout << "(RK) Exact Match Found: " << RK_exact_matches[i].GetText() << " At: " << RK_exact_matches[i].GetCharIndex() << " From: " << RK_exact_matches[i].GetDocumentName() << endl;
         cout << "\n";
     }
+    cout << "-----------------------------------------------------------------------------------------------------------" << endl;
+    float RK_plagiarismPercentage = (RK_PlagiarizedCharCount / totalCharCount) * 100;
+    float BF_plagiarismPercentage = (BF_PlagiarizedCharCount / totalCharCount) * 100;
+
+    float avgPlagiarism = (RK_plagiarismPercentage + BF_plagiarismPercentage) / 2;
+
+    cout << RK_PlagiarizedCharCount << endl;
+    cout << BF_PlagiarizedCharCount << endl;
+    cout << totalCharCount << endl;
+
+    cout << "---------------------------------------- " << endl;
+    cout << "Total Plagiarism Detected: " << avgPlagiarism << "%" << endl;
+    cout << "---------------------------------------- " << endl;
 }
